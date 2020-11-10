2fe6eaa1d66c9d2a1bfdc9a4825520ab7e41633c
spatte22 <42690127+spatte22@users.noreply.github.com>
2020-11-10 00:05:26 -0700

    Initial commit

---
+++ cookingLanguage/margJS.js
@@ -0,0 +1,50 @@
+console.log('Hello world!');
+
+
+
+
+// Drop-down function
+var ddValue = document.getElementById('ddBioContent');
+
+function ddBio() { 
+	if(ddValue.style.display == 'none') {
+    ddValue.style.display = '';
+  }else if(ddValue.style.display == '') {
+    ddValue.style.display = 'none';
+  }
+}
+
+function contactEmail() 
+{
+    window.open("mailto:margaritainesp@hotmail.com?subject=Contact:%20Margarita%20Papakosta&body=Hi%20Marg,%0A%0A%0A");
+}
+
+function contactTutorEmail() {
+	window.open("mailto:margaritainesp@hotmail.com?subject=Tutor%20Inquiry&body=Hi%20Marg,%0A%0AMy%20Name:%0ADesired%20Language:%0ACurrent%20Level:%0AMy%20Availability:%0APotential%20Tutor%20Package:%0A%0A%0A");
+}
+
+//language tutor slides
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  
+}
