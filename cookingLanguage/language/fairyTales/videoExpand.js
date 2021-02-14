a899dd042f11f010fcdff1816cb5eb00c7f63c5a
spatte22 <sean.patterson1989@gmail.com>
2021-02-14 13:47:09 -0700

    ox/frog translation and bellingcat video

--- cookingLanguage/language/fairyTales/videoExpand.js
+++ cookingLanguage/language/fairyTales/videoExpand.js
@@ -2,10 +2,19 @@ console.log("iframe working");
 
 const expandButton = document.getElementsByClassName('iframeExpand');
 const iframeContainer = document.getElementsByClassName('stickyIframe');
+
+window.onscroll = scrollFunction;
+function scrollFunction () {
+	if (window.pageYOffset > 200){
+		console.log('scrolling!');
+		document.getElementById('videoButton').style.display = 'block';
+	}
+};
+
 let a;
 for(a=0; a < expandButton.length; a++) {
 	expandButton[a].onclick = function() {
 		this.parentElement.classList.toggle("hide");
 	}
-}
+};
 
